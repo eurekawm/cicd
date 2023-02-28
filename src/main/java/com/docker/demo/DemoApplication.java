fcf01f4dd317e644358ee7950c0e000c4b8e3edb
zhangyfg <zhangyfg@rrxplatform.com>
2023-02-28 21:40:55 +0800

    init

---
+++ src/main/java/com/docker/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "world";
+    }
+
+}
